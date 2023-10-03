54d361cedd29b7c37ad80915fe64ad74be8af5bb
JandroThielman <9020498@student.zadkine.nl>
2023-10-03 10:09:21 +0200

    Jandro

---
+++ Javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("1").style.color = "blue";
+document.body.style.backgroundColor = "red";
+document.getElementById("1").style.fontSize = "200px";
+document.getElementById("1").style.textAlign = "center";
+document.getElementById("1").style.marginTop = "16%";
